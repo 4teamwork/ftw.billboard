284c62d0b9085edeb13b1af30a31935204961676
Julian Infanger <j.infanger@4teamwork.ch>
2011-02-07 12:15:12 +0100

    removed unused function, replaced getPath by getURL

--- ftw/billboard/browser/ad.py
+++ ftw/billboard/browser/ad.py
@@ -23,21 +23,5 @@ class AdView(BrowserView):
             'portal_type': 'Image',
         }
         for brain in catalog(query):
-            elements.append(brain.getPath())
-        return elements
-
-    def get_files(self):
-        elements = []
-        catalog = self.context.portal_catalog
-        query = {
-            'path': '/'.join(self.context.getPhysicalPath()),
-            'portal_type': 'File',
-        }
-        for brain in catalog(query):
-            obj = brain.getObject()
-            elements.append({
-                'title': brain.Title,
-                'path': '%s/at_download/file' % brain.getPath(),
-                'icon': obj.getIcon(),
-            })
+            elements.append(brain.getURL())
         return elements
