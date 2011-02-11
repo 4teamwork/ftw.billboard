44aa2901697223cb303f56a6d6bd947141d4daa5
Thomas Buchberger <t.buchberger@4teamwork.ch>
2011-02-11 12:43:01 +0100

    Also show ad of translated categories in category view.

--- ftw/billboard/browser/category.py
+++ ftw/billboard/browser/category.py
@@ -1,3 +1,4 @@
+from Acquisition import aq_inner
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
@@ -16,9 +17,26 @@ class CategoryView(BrowserView):
         return self.template()
 
     def get_ads(self):
-        """returns all ads in this category"""
-        catalog = self.context.portal_catalog
-        return catalog(dict(
-            path = '/'.join(self.context.getPhysicalPath()),
-            portal_type = 'BillboardAd',
-            sort_on = 'created'))
+        """Returns all ads in this category."""
+        context = aq_inner(self.context)
+        ct = getToolByName(context, 'portal_catalog')
+
+        # TODO: make it configurable if ads in all translated categories
+        # should be shown.
+        if not hasattr(context, 'getTranslations'):
+            return ct(
+                path = '/'.join(self.context.getPhysicalPath()),
+                portal_type = 'BillboardAd',
+                sort_on = 'created',
+                sort_order = 'descending',
+            )
+        else:
+            translations = context.getTranslations().values()
+            paths = ['/'.join(t[0].getPhysicalPath()) for t in translations]
+            return ct(
+                path=paths,
+                portal_type='BillboardAd',
+                Language='all',
+                sort_on='created',
+                sort_order = 'descending',
+            )
