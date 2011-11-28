4d896b8359931bcc89375d4a7585d2961e4f2c9e
Jonas Baumann <jone@jone.ch>
2011-11-28 17:19:33 +0100

    Rename readme to .rst

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='ftw.billboard',
       version=version,
       description='Billboard for plone for publishing advertisements.',
       long_description=open('README.txt').read() + '\n' +
-                       open(os.path.join('docs', 'HISTORY.txt')).read(),
+                       open(os.path.join('docs', 'HISTORY.rst')).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         'Framework :: Plone',
